a687ebf21dc4315e24d923b580722f9fd758e58f
sukruaydin <sukraydin29@gmail.com>
2022-11-13 10:36:39 +0300

    Initial commit

---
+++ src/test/java/com/cydeo/day01/SimpleGetRequest.java
@@ -0,0 +1,29 @@
+package com.cydeo.day01;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+    //normally it is not like this, just for today
+    String url = "http://54.82.123.95:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.get(url);
+        int statusCode = response.statusCode(); //200
+
+        //printing statusCode
+        System.out.println("statusCode = " + statusCode);
+
+        //printing response body
+        response.prettyPrint();
+
+
+
+    }
+
+}
