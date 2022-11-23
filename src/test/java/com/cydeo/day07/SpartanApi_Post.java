18945018de8abbe5508edea31dddc4c088b659d1
sukruaydin <sukraydin29@gmail.com>
2022-11-23 13:39:36 +0300

    changes added

--- src/test/java/com/cydeo/day07/SpartanApi_Post.java
+++ src/test/java/com/cydeo/day07/SpartanApi_Post.java
@@ -2,6 +2,8 @@ package com.cydeo.day07;
 
 import com.cydeo.pojo.Spartan;
 import com.cydeo.utilities.SpartanTestBase;
+import com.cydeo.utilities.SpartanUtils;
+import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -137,7 +139,6 @@ public class SpartanApi_Post extends SpartanTestBase {
         spartan.setPhone(8877445596l);
         //id is sent as 0 by default
 
-
         Response response = given().accept(ContentType.JSON)
                 .and().contentType(ContentType.JSON)
                 //auto-serialization by body() method
@@ -155,6 +156,76 @@ public class SpartanApi_Post extends SpartanTestBase {
         assertThat(response.path("data.phone"), is(8877445596l));
 
         response.prettyPrint();
+    }
+
+    @DisplayName("SpartanUtil practice")
+    @Test
+    public void test4(){
+
+        Response response = given().accept(ContentType.JSON)
+                .and().contentType(ContentType.JSON)
+                //auto-serialization by body() method
+                .body(SpartanUtils.spartanMap())
+                .when().post("/api/spartans")
+                .then().statusCode(201)
+                .contentType("application/json")
+                .extract().response();
+
+        //continue asserting
+        String expectedMessage = "A Spartan is Born!";
+        assertThat(response.path("success"), is(expectedMessage));
+
+        response.prettyPrint();
+    }
+
+    @DisplayName("everything recaped, serialization, deserialization, hamcrest")
+    @Test
+    public void test5(){
+/*
+            Given accept type and Content type is JSON
+            And request json body is:
+            {
+              "gender":"Male",
+              "name":"Severus",
+              "phone":8877445596
+           }
+            When user sends POST request to '/api/spartans'
+            Then status code 201
+            And content type should be application/json
+            And json payload/response/body should contain:
+            "A Spartan is Born!" message
+            and same data what is posted
+        */
+        Spartan spartan = new Spartan();
+        spartan.setName("Hatice");
+        spartan.setGender("Female");
+        spartan.setPhone(8877445596l);
+        //id is sent as 0 by default
+
+        String expectedMessage = "A Spartan is Born!";
+
+        int idFromPost = given().accept(ContentType.JSON)
+                .and().contentType(ContentType.JSON)
+                //auto-serialization by body() method
+                .body(spartan)
+                .when().post("/api/spartans")
+                .then().statusCode(201)
+                .contentType("application/json")
+                .body("success", is(expectedMessage))
+                .extract().jsonPath().getInt("data.id");
+
+        System.out.println("idFromPost = " + idFromPost);
+
+        Spartan spartanPosted = given().pathParam("id", idFromPost)
+                .get("/api/spartans/{id}")
+                .then().statusCode(200)
+                .extract().response().as(Spartan.class);
+
+        assertThat(spartanPosted.getName(),is(spartan.getName()));
+        assertThat(spartanPosted.getGender(),is(spartan.getGender()));
+        assertThat(spartanPosted.getPhone(),is(spartan.getPhone()));
+        assertThat(spartanPosted.getId(),is(idFromPost));
+
     }
 
 }
--- src/test/java/com/cydeo/pojo/Spartan.java
+++ src/test/java/com/cydeo/pojo/Spartan.java
@@ -1,5 +1,6 @@
 package com.cydeo.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,6 +8,11 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+//by sending post request, i don't need to specify id value
+//with this annotation i can ignore id key
+//excluded for serialization, but still included for de-serialization
+//also we can create another class with 3 instances, but it doesn't make sense since we have solution
+@JsonIgnoreProperties(value = "id", allowSetters = true)
 public class Spartan {
 
     //fields for a spartan
@@ -15,42 +21,6 @@ public class Spartan {
     private String gender;
     private long phone;
 
-/*    //getters & setters
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getGender() {
-        return gender;
-    }
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-    public long getPhone() {
-        return phone;
-    }
-    public void setPhone(long phone) {
-        this.phone = phone;
-    }
-
-    //toString method
-    @Override
-    public String toString() {
-        return "Spartan{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", phone=" + phone +
-                '}';
-    }*/
 
 
 }
