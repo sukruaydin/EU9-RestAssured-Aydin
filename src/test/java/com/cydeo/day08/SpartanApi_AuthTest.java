aec56fe4a658bb980fcc17f7260f4da8057d0750
sukruaydin <sukraydin29@gmail.com>
2022-11-23 17:21:08 +0300

    changes added

---
+++ src/test/java/com/cydeo/day08/SpartanApi_AuthTest.java
@@ -0,0 +1,49 @@
+package com.cydeo.day08;
+
+import com.cydeo.utilities.SpartanAuthTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+public class SpartanApi_AuthTest extends SpartanAuthTestBase {
+
+    @DisplayName("as")
+    @Test
+    public void test1(){
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @DisplayName("as")
+    @Test
+    public void test2(){
+
+
+    }
+
+    @DisplayName("as")
+    @Test
+    public void test3(){
+
+
+    }
+
+}
---
+++ src/test/java/com/cydeo/utilities/SpartanAuthTestBase.java
@@ -0,0 +1,24 @@
+package com.cydeo.utilities;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
+public class SpartanAuthTestBase {
+
+    @BeforeAll
+    public static void init() {
+        baseURI = ConfigurationReader.getProperty("spartan_auth_ip_address");
+
+        //String dbUrl = "jdbc:oracle:thin:@54.82.123.95:1521:xe";
+        //String dbUsername = "SP";
+        //String dbPassword = "SP";
+        //DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+    }
+
+    @AfterAll
+    public static void teardown(){
+        //DBUtils.destroy();
+    }
+}
