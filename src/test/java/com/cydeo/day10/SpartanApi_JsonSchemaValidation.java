8ea43eb40f908771c08873040e4645700d137473
sukruaydin <sukraydin29@gmail.com>
2022-11-28 18:39:13 +0300

    changes added

--- src/test/java/com/cydeo/day10/SpartanApi_JsonSchemaValidation.java
+++ src/test/java/com/cydeo/day10/SpartanApi_JsonSchemaValidation.java
@@ -2,10 +2,13 @@ package com.cydeo.day10;
 
 import com.cydeo.utilities.SpartanAuthTestBase;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -20,31 +23,44 @@ public class SpartanApi_JsonSchemaValidation extends SpartanAuthTestBase {
                 .and().auth().basic("admin","admin")
                 .when().get("/api/spartans/{id}")
                 .then().statusCode(200)
-                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SingleSpartanSchema.json"))
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SingleSpartanJsonSchema.json"))
                 .log().all();
 
     }
 
-    @DisplayName("vv")
+    @DisplayName("JsonSchemaValidator class, matchesJsonSchema method")
     @Test
     public void test2(){
 
-       //AllSpartanSchema is not exist, so I couldn't do this
-
-        /*
-
-        COPIED
-
-        given()
-                .accept(ContentType.JSON)
-                .auth().basic("admin","admin")
-        .when()
-                .get("/api/spartans")
-        .then()
-                .statusCode(200)
+        given().accept(ContentType.JSON)
+                .and().auth().basic("admin","admin")
+                .when().get("/api/spartans")
+                .then().statusCode(200)
                 //what if you have your .json file not under resources following way -->
-                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/com/cybertek/day10/allSpartansSchema.json")));
-         */
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/com/cydeo/day10/AllSpartansJsonSchema.json")));
 
     }
+
+    @DisplayName("Homework")
+    @Test
+    public void test3(){
+
+        /*
+            homework
+            put your post json schema under day10
+            post one spartan using dynamic input(name,gender,phone)
+            verify your post response matching with json schema
+         */
+
+
+        given().accept(ContentType.JSON)
+                .and().auth().basic("admin","admin")
+                .when().get("/api/spartans")
+                .then().statusCode(200)
+                //what if you have your .json file not under resources following way -->
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/com/cydeo/day10/AllSpartansJsonSchema.json")));
+
+    }
+
+
 }
