31fdf1d0408a070b09885cc86988035315fcf75c
sukruaydin <sukraydin29@gmail.com>
2022-12-12 12:25:00 +0300

    changes added

--- src/test/java/com/cydeo/day12/SpartanAuthApi_SpecTest.java
+++ src/test/java/com/cydeo/day12/SpartanAuthApi_SpecTest.java
@@ -5,6 +5,7 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
@@ -12,7 +13,10 @@ import static io.restassured.RestAssured.*;
 //java_class_3
 public class SpartanAuthApi_SpecTest extends SpartanAuthNewTestBase {
 
-    /*
+    @DisplayName("requestSpec, responseSpec by creation")
+    @Test
+    public void test1(){
+        /*
         GET request
 
         all tests in this class will use admin credentials
@@ -21,9 +25,6 @@ public class SpartanAuthApi_SpecTest extends SpartanAuthNewTestBase {
         all test in this class response status code is 200
         all test in this class response content type header is json
      */
-
-    @Test
-    public void test1(){
 
         RequestSpecification requestSpec = given().accept(ContentType.JSON)
                                             .and().auth().basic("admin", "admin");
@@ -38,6 +39,7 @@ public class SpartanAuthApi_SpecTest extends SpartanAuthNewTestBase {
 
     }
 
+    @DisplayName("requestSpec, responseSpec by Inheritance")
     @Test
     public void test2(){
 
@@ -48,6 +50,7 @@ public class SpartanAuthApi_SpecTest extends SpartanAuthNewTestBase {
 
     }
 
+    @DisplayName("requestSpec, responseSpec added other verifications")
     @Test
     public void test3(){
         given().spec(requestSpec)
